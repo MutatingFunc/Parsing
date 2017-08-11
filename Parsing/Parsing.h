72f93d379acda392f79a2bd1a38be14e78bfa1d4
James <j.froggatt-15@student.lboro.ac.uk>
2017-08-11 21:18:19 +0100

    Initial Commit

---
+++ Parsing/Parsing.h
@@ -0,0 +1,19 @@
+//
+//  Parsing.h
+//  Parsing
+//
+//  Created by James Froggatt on 11.08.2017.
+//  Copyright © 2017 James Froggatt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Parsing.
+FOUNDATION_EXPORT double ParsingVersionNumber;
+
+//! Project version string for Parsing.
+FOUNDATION_EXPORT const unsigned char ParsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parsing/PublicHeader.h>
+
+
