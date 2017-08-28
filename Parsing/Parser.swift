cce1bd8641dea22c67f729443eff074812e046d1
James <j.froggatt-15@student.lboro.ac.uk>
2017-08-28 15:13:23 +0100

    Functional

--- Parsing/Parser.swift
+++ Parsing/Parser.swift
@@ -8,78 +8,52 @@
 
 private var whitespaceParser: Parser<()>? = nil
 public struct Parser<Token> {
-	public var prefix: [PrefixMatchable], ws: [PrefixMatchable], parse: (Substring) -> ParserResult<Token>
-	public init(prefix: [PrefixMatchable], ws: [PrefixMatchable], parse: @escaping (Substring) -> ParserResult<Token>) {
-		self.prefix = prefix; self.ws = ws; self.parse = parse
+	public var parse: (inout Substring) -> Token?
+	public init(_ parse: @escaping (inout Substring) -> Token?) {
+		self.parse = parse
 	}
-	public static func applyingIgnoreRules(prefix: [PrefixMatchable], ws: [PrefixMatchable], parse: @escaping (Substring) -> ParserResult<Token>) -> Parser {
-		return Parser(prefix: prefix, ws: ws) {
-			var substr = $0
-			while case .parsed(_, let rest)? = whitespaceParser?.parse(substr) {substr = rest}
-			return parse(substr)
+	public func skippingWhitespace() -> Parser {
+		return Parser {substr in
+			while whitespaceParser?.parse(&substr) != nil {}
+			return self.parse(&substr)
 		}
-	}
-	public static func applyingIgnoreRules(_ parser: Parser) -> Parser {
-		return Parser.applyingIgnoreRules(prefix: parser.prefix, ws: parser.ws, parse: parser.parse)
 	}
 	
-	public func ignoring(prefix parser: Parser<()>?) -> Parser {
-		let parser = parser?.ignoring(prefix: nil)
-		return Parser(prefix: self.prefix, ws: parser?.prefix ?? []) {[parse] in
+	public func withWhitespace(_ whitespace: Parser<()>?) -> Parser {
+		return Parser {substr in
 			let oldValue = whitespaceParser
-			whitespaceParser = parser
+			whitespaceParser = whitespace
 			defer {whitespaceParser = oldValue}
-			return parse($0)
+			return self.parse(&substr)
 		}
-	}
-	public func map<Result>(_ transform: @escaping (Token) -> Result) -> Parser<Result> {
-		let parse = self.parse
-		return .init(prefix: self.prefix, ws: self.ws, parse: {parse($0).map(transform)})
 	}
 	
 	public func parseToEnd(_ file: String) throws -> Token {
 		let startTime = CFAbsoluteTimeGetCurrent()
 		defer {print("\n\nTime to parse: \(CFAbsoluteTimeGetCurrent() - startTime)\n\n")}
-		let parser = self → (/"$" ~> ())
-		switch parser.parse(Substring(file)) {
-		case .parsed((let token, _), _): return token
-		case .failed(let rest):
-			var line = 1, position: Int = 0
-			if file.startIndex != rest.startIndex {
-				let indexIterator = sequence(first: file.startIndex) {prev in
-					let next = file.index(after: prev)
-					return next == rest.startIndex ? nil : next
-				}
-				for index in indexIterator {
-					if file[index] == "\n" {line += 1; position = 0}
-					else {position += 1}
+		
+		let parser = self → (/"$").parser ~> {ast, _ in ast}
+		var rest = Substring(file)
+		if let result = parser.parse(&rest) {
+			return result
+		}
+		
+		for (lineNum, line) in zip(1..., file.split(separator: "\n")) {
+			for (linePos, charIndex) in zip(0..., line.characters.indices) {
+				if charIndex == rest.startIndex {
+					throw ParserError.failed(rest: rest, line: lineNum, position: linePos)
 				}
 			}
-			let error = ParserError.failed(expected: parser.prefix, ws: parser.ws, got: rest, line: line, position: position)
-			print("Error: " + error.localizedDescription)
-			throw error
 		}
-	}
-}
-public enum ParserResult<Token> {
-	case parsed(Token, rest: Substring)
-	case failed(got: Substring)
-	
-	public func map<Result>(_ transform: (Token) -> Result) -> ParserResult<Result> {
-		switch self {
-		case .parsed(let token, let rest): return .parsed(transform(token), rest: rest)
-		case .failed(let rest): return .failed(got: rest)
-		}
+		throw ParserError.failed(rest: rest, line: 1, position: 0)
 	}
 }
 
 public enum ParserError: LocalizedError {
-	case failed(expected: [PrefixMatchable], ws: [PrefixMatchable], got: Substring, line: Int, position: Int)
+	case failed(rest: Substring, line: Int, position: Int)
 	public var localizedDescription: String {
 		switch self {
-		case let .failed(matchables, whitespace, rest, line, index):
-			let wsMessage = whitespace.isEmpty ? "With no whitespace permitted" : "Or any whitespace: \(whitespace)"
-			return "Line \(line) position \(index)\n\nTop level expects one of: \(matchables),\n\(wsMessage)\n\nRemaining input:\n\(rest)"
+		case let .failed(rest, line, index): return "Line \(line) position \(index)\n\nRemaining input:\n\(rest)"
 		}
 	}
 }
@@ -88,131 +62,127 @@ precedencegroup ParsingPrecedence {
 	lowerThan: AdditionPrecedence
 	higherThan: WhitespacePrecedence
 }
-postfix operator ~>
-public postfix func ~>(prefix: PrefixMatchable) -> Parser<Substring> {
-	return Parser(prefix: [prefix], ws: []) {
-		guard let (matched, rest) = prefix.take(from: $0) else {return .failed(got: $0)}
-		return .parsed(matched, rest: rest)
-	}
-}
 infix operator ~>: ParsingPrecedence
 public func ~><Token>(prefix: PrefixMatchable, token: @escaping (Substring) -> Token) -> Parser<Token> {
-	return Parser(prefix: [prefix], ws: []) {
-		guard let (matched, rest) = prefix.take(from: $0) else {return .failed(got: $0)}
-		return .parsed(token(matched), rest: rest)
-	}
+	return prefix.parser ~> token
 }
 public func ~><Token>(prefix: PrefixMatchable, token: Token) -> Parser<Token> {
-	return Parser(prefix: [prefix], ws: []) {
-		guard let (_, rest) = prefix.take(from: $0) else {return .failed(got: $0)}
-		return .parsed(token, rest: rest)
+	return Parser {substr in
+		prefix.parse(from: &substr) != nil ? token : nil
 	}
 }
 public func ~><Token, AST>(parser: Parser<Token>, transform: @escaping (Token) -> AST) -> Parser<AST> {
-	return parser.map(transform)
+	return Parser {substr in
+		parser.parse(&substr).map(transform)
+	}
 }
 
 precedencegroup WhitespacePrecedence {}
 infix operator --: WhitespacePrecedence
 public func --<Token>(parser: Parser<Token>, whitespace: Parser<()>) -> Parser<Token> {
-	return parser.ignoring(prefix: whitespace)
+	return parser.withWhitespace(whitespace)
 }
 
 prefix operator -
 public prefix func -<Token>(parser: Parser<Token>) -> Parser<Token> {
-	return Parser.applyingIgnoreRules(parser)
+	return parser.skippingWhitespace()
 }
 infix operator →: AdditionPrecedence
-public func →(lhs: Parser<()>, rhs: PrefixMatchable) -> Parser<()> {
-	return lhs → (rhs ~> ()) ~> {_ in ()}
-}
 public func →<TokenA, TokenB>(lhs: Parser<TokenA>, rhs: Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: lhs.prefix, ws: lhs.ws) {
-		switch lhs.parse($0) {
-		case .failed(let rest): return .failed(got: rest)
-		case .parsed(let token1, let substr):
-			switch rhs.parse(substr) {
-			case .failed(let rest): return .failed(got: rest)
-			case .parsed(let token2, let rest): return .parsed((token1, token2), rest: rest)
-			}
+	return Parser {substr in
+		if let token1 = lhs.parse(&substr), let token2 = rhs.parse(&substr) {
+			return (token1, token2)
 		}
+		return nil
 	}
-}
-public func →<TokenA, TokenB>(lhs: @escaping () -> Parser<TokenA>, rhs: Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: [], ws: []) {(lhs() → rhs).parse($0)} //can't get prefix without resolving
-}
-public func →<TokenA, TokenB>(lhs: Parser<TokenA>, rhs: @escaping () -> Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: lhs.prefix, ws: lhs.ws) {(lhs → rhs()).parse($0)}
-}
-public func →<TokenA, TokenB>(lhs: @escaping () -> Parser<TokenA>, rhs: @escaping () -> Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: [], ws: []) {(lhs() → rhs()).parse($0)}
 }
 
 infix operator -→: AdditionPrecedence
 public func -→<TokenA, TokenB>(lhs: Parser<TokenA>, rhs: Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
 	return lhs → -rhs
 }
-public func -→<TokenA, TokenB>(lhs: @escaping () -> Parser<TokenA>, rhs: Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: [], ws: []) {(lhs() -→ rhs).parse($0)} //can't get prefix without resolving
-}
-public func -→<TokenA, TokenB>(lhs: Parser<TokenA>, rhs: @escaping () -> Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: lhs.prefix, ws: lhs.ws) {(lhs -→ rhs()).parse($0)}
-}
-public func -→<TokenA, TokenB>(lhs: @escaping () -> Parser<TokenA>, rhs: @escaping () -> Parser<TokenB>) -> Parser<(TokenA, TokenB)> {
-	return Parser(prefix: [], ws: []) {(lhs() -→ rhs()).parse($0)} //can't get prefix without resolving
-}
 
 public func |<Token>(lhs: Parser<Token>, rhs: Parser<Token>) -> Parser<Token> {
-	return Parser(prefix: lhs.prefix + rhs.prefix, ws: lhs.ws + rhs.ws) {
-		switch lhs.parse($0) {
-		case .parsed(let token1, let rest): return .parsed(token1, rest: rest)
-		case .failed(_):
-			switch rhs.parse($0) {
-			case .parsed(let token2, let rest): return .parsed(token2, rest: rest)
-			case .failed(let rest): return .failed(got: rest)
-			}
-		}
+	return Parser {substr in
+		let original = substr
+		if let token = lhs.parse(&substr) {return token}
+		substr = original
+		if let token = rhs.parse(&substr) {return token}
+		substr = original
+		return nil
 	}
 }
 
 postfix operator .?
 public postfix func .?<Token>(parser: Parser<Token>) -> Parser<Token?> {
-	return Parser(prefix: parser.prefix, ws: parser.ws) {
-		if case let .parsed(token, rest) = parser.parse($0) {return .parsed(token, rest: rest)}
-		else {return .parsed(nil, rest: $0)}
-	}
-}
-postfix operator *
-public postfix func *<Token>(parser: Parser<Token>) -> Parser<[Token]> {
-	return Parser(prefix: parser.prefix, ws: parser.ws) {
-		var str = $0, tokens: [Token] = []
-		while case let .parsed(token, rest) = parser.parse(str) {
-			str = rest
-			tokens.append(token)
-		}
-		return .parsed(tokens, rest: str)
-	}
-}
-postfix operator -*
-public postfix func -*<Token>(parser: Parser<Token>) -> Parser<[Token]> {
-	return Parser(prefix: parser.prefix, ws: parser.ws) {
-		var str = $0, tokens: [Token] = []
-		while case let .parsed(token, rest) = parser.parse(str) {
-			str = rest
-			tokens.append(token)
-			while case .parsed(_, let rest)? = whitespaceParser?.parse(str) {str = rest}
-		}
-		return .parsed(tokens, rest: str)
+	return Parser {substr in
+		let original = substr
+		if let token = parser.parse(&substr) {return Optional(token)}
+		substr = original
+		return Optional(nil)
 	}
 }
 
 postfix operator +
 public postfix func +<Token>(parser: Parser<Token>) -> Parser<[Token]> {
-	return parser → parser* ~> {first, rest in [first] + rest}
+	return Parser {substr in
+		guard let first = parser.parse(&substr) else {return nil}
+		var tokens = [first]
+		var rest = substr
+		while let nextToken = parser.parse(&substr) {
+			tokens.append(nextToken)
+			rest = substr
+		}
+		substr = rest
+		return tokens
+	}
 }
 postfix operator -+
 public postfix func -+<Token>(parser: Parser<Token>) -> Parser<[Token]> {
-	return parser -→ parser-* ~> {first, rest in [first] + rest}
+	return Parser {substr in
+		guard let first = parser.parse(&substr) else {return nil}
+		var tokens = [first]
+		var rest = substr
+		let parser = parser.skippingWhitespace()
+		while let nextToken = parser.parse(&substr) {
+			tokens.append(nextToken)
+			rest = substr
+		}
+		substr = rest
+		return tokens
+	}
+}
+
+postfix operator *
+public postfix func *<Token>(parser: Parser<Token>) -> Parser<[Token]> {
+	return Parser {substr in
+		var tokens: [Token] = []
+		var rest = substr
+		while let nextToken = parser.parse(&substr) {
+			tokens.append(nextToken)
+			rest = substr
+		}
+		substr = rest
+		return tokens
+	}
+}
+postfix operator -*
+public postfix func -*<Token>(parser: Parser<Token>) -> Parser<[Token]> {
+	return Parser {substr in
+		var tokens: [Token] = []
+		var rest = substr
+		if let first = parser.parse(&substr) {
+			tokens.append(first)
+			rest = substr
+			let parser = parser.skippingWhitespace()
+			while let nextToken = parser.parse(&substr) {
+				tokens.append(nextToken)
+				rest = substr
+			}
+		}
+		substr = rest
+		return tokens
+	}
 }
 
 
@@ -221,49 +191,25 @@ public postfix func -+<Token>(parser: Parser<Token>) -> Parser<[Token]> {
 public func →<TokenA, TokenB, TokenZ>(lhs: Parser<(TokenA, TokenB)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenZ)> {
 	return lhs → rhs ~> {($0.0.0, $0.0.1, $0.1)}
 }
-public func →<TokenA, TokenB, TokenZ>(lhs: Parser<(TokenA, TokenB)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenZ)> {
-	return lhs → rhs ~> {($0.0.0, $0.0.1, $0.1)}
-}
 public func →<TokenA, TokenB, TokenC, TokenZ>(lhs: Parser<((TokenA, TokenB), TokenC)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenZ)> {
-	return lhs → rhs ~> {($0.0.0.0, $0.0.0.1, $0.0.1, $0.1)}
-}
-public func →<TokenA, TokenB, TokenC, TokenZ>(lhs: Parser<((TokenA, TokenB), TokenC)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenZ)> {
 	return lhs → rhs ~> {($0.0.0.0, $0.0.0.1, $0.0.1, $0.1)}
 }
 public func →<TokenA, TokenB, TokenC, TokenD, TokenZ>(lhs: Parser<(((TokenA, TokenB), TokenC), TokenD)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenZ)> {
 	return lhs → rhs ~> {($0.0.0.0.0, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
 }
-public func →<TokenA, TokenB, TokenC, TokenD, TokenZ>(lhs: Parser<(((TokenA, TokenB), TokenC), TokenD)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenZ)> {
-	return lhs → rhs ~> {($0.0.0.0.0, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
-}
 public func →<TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ>(lhs: Parser<((((TokenA, TokenB), TokenC), TokenD), TokenE)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ)> {
-	return lhs → rhs ~> {($0.0.0.0.0.0, $0.0.0.0.0.1, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
-}
-public func →<TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ>(lhs: Parser<((((TokenA, TokenB), TokenC), TokenD), TokenE)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ)> {
 	return lhs → rhs ~> {($0.0.0.0.0.0, $0.0.0.0.0.1, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
 }
 
 public func -→<TokenA, TokenB, TokenZ>(lhs: Parser<(TokenA, TokenB)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenZ)> {
 	return lhs -→ rhs ~> {($0.0.0, $0.0.1, $0.1)}
 }
-public func -→<TokenA, TokenB, TokenZ>(lhs: Parser<(TokenA, TokenB)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenZ)> {
-	return lhs -→ rhs ~> {($0.0.0, $0.0.1, $0.1)}
-}
 public func -→<TokenA, TokenB, TokenC, TokenZ>(lhs: Parser<((TokenA, TokenB), TokenC)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenZ)> {
-	return lhs -→ rhs ~> {($0.0.0.0, $0.0.0.1, $0.0.1, $0.1)}
-}
-public func -→<TokenA, TokenB, TokenC, TokenZ>(lhs: Parser<((TokenA, TokenB), TokenC)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenZ)> {
 	return lhs -→ rhs ~> {($0.0.0.0, $0.0.0.1, $0.0.1, $0.1)}
 }
 public func -→<TokenA, TokenB, TokenC, TokenD, TokenZ>(lhs: Parser<(((TokenA, TokenB), TokenC), TokenD)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenZ)> {
 	return lhs -→ rhs ~> {($0.0.0.0.0, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
 }
-public func -→<TokenA, TokenB, TokenC, TokenD, TokenZ>(lhs: Parser<(((TokenA, TokenB), TokenC), TokenD)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenZ)> {
-	return lhs -→ rhs ~> {($0.0.0.0.0, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
-}
 public func -→<TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ>(lhs: Parser<((((TokenA, TokenB), TokenC), TokenD), TokenE)>, rhs: Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ)> {
-	return lhs -→ rhs ~> {($0.0.0.0.0.0, $0.0.0.0.0.1, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
-}
-public func -→<TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ>(lhs: Parser<((((TokenA, TokenB), TokenC), TokenD), TokenE)>, rhs: @escaping () -> Parser<TokenZ>) -> Parser<(TokenA, TokenB, TokenC, TokenD, TokenE, TokenZ)> {
 	return lhs -→ rhs ~> {($0.0.0.0.0.0, $0.0.0.0.0.1, $0.0.0.0.1, $0.0.0.1, $0.0.1, $0.1)}
 }
--- Parsing/PrefixMatchables.swift
+++ Parsing/PrefixMatchables.swift
@@ -9,46 +9,53 @@
 import Foundation
 
 public protocol PrefixMatchable: CustomStringConvertible {
-	func matchPrefix(_ str: Substring) -> Range<String.Index>?
+	func matchPrefix(_ str: Substring) -> String.Index?
 }
 public extension PrefixMatchable {
-	func take(from str: Substring) -> (matched: Substring, rest: Substring)? {
-		guard let match = self.matchPrefix(str) else {return nil}
-		assert(match.lowerBound == str.startIndex)
-		return (matched: str[match], rest: str[match.upperBound...])
+	func parse(from str: inout Substring) -> Substring? {
+		guard let matchEnd = self.matchPrefix(str) else {return nil}
+		assert(str.indices.contains(matchEnd))
+		defer {str.removeSubrange(..<matchEnd)}
+		return str[..<matchEnd]
 	}
+	var parser: Parser<Substring> {return Parser(self.parse)}
+}
+public protocol ManyPrefixMatchable: PrefixMatchable {
+	func matchManyPrefix(_ str: Substring) -> String.Index?
 }
 
 public struct MatchableString: PrefixMatchable {
-	public var literal: String, caseSensitive: Bool, matchMany: Bool
-	public func matchPrefix(_ str: Substring) -> Range<String.Index>? {
+	public var literal: String, caseSensitive: Bool
+	public func matchPrefix(_ str: Substring) -> String.Index? {
 		var options: String.CompareOptions = [.anchored]
 		if !caseSensitive {options.insert(.caseInsensitive)}
-		let range = str.range(of: literal, options: options)
-		assert(range.map{$0.lowerBound.encodedOffset == 0} ?? true, "fix for Substring.range result beginning at 0, not .startIndex is no longer needed")
-		return range.map{str.startIndex ..< str.index(str.startIndex, offsetBy: $0.upperBound.encodedOffset)}
+		guard let range = str.range(of: literal, options: options) else {return nil}
+		assert(range.lowerBound.encodedOffset == 0, "bug where Substring.range result begins at 0, not .startIndex appears to be fixed")
+		//assert(range.lowerBound == str.startIndex, "anchored search should only match prefix)
+		return str.index(str.startIndex, offsetBy: range.upperBound.encodedOffset)
 	}
 	public var description: String {
-		return (caseSensitive ? "^" : "~^") + "\"" + literal.description + "\""
+		return (caseSensitive ? "^" : "~^") + "\"\(literal.description)\""
 	}
 }
 prefix operator ^
-public prefix func ^(str: String) -> MatchableString {return MatchableString(literal: str, caseSensitive: true, matchMany: false)}
+public prefix func ^(str: String) -> MatchableString {return MatchableString(literal: str, caseSensitive: true)}
 prefix operator ~^
-public prefix func ~^(str: String) -> MatchableString {return MatchableString(literal: str, caseSensitive: false, matchMany: false)}
+public prefix func ~^(str: String) -> MatchableString {return MatchableString(literal: str, caseSensitive: false)}
 
 
 public struct RegEx: PrefixMatchable {
 	public var regEx: String, caseSensitive: Bool
-	public func matchPrefix(_ str: Substring) -> Range<String.Index>? {
+	public func matchPrefix(_ str: Substring) -> String.Index? {
 		var options: String.CompareOptions = [.anchored, .regularExpression]
 		if !caseSensitive {options.insert(.caseInsensitive)}
-		let range = str.range(of: regEx, options: options)
-		assert(range.map{$0.lowerBound.encodedOffset == 0} ?? true, "fix for Substring.range result beginning at 0, not .startIndex is no longer needed")
-		return range.map{str.startIndex ..< str.index(str.startIndex, offsetBy: $0.upperBound.encodedOffset)}
+		guard let range = str.range(of: regEx, options: options) else {return nil}
+		assert(range.lowerBound.encodedOffset == 0, "bug where Substring.range result begins at 0, not .startIndex appears to be fixed")
+		//assert(range.lowerBound == str.startIndex, "anchored search should only match prefix)
+		return str.index(str.startIndex, offsetBy: range.upperBound.encodedOffset)
 	}
 	public var description: String {
-		return (caseSensitive ? "/" : "~/") + "\"" + regEx + "\""
+		return (caseSensitive ? "/" : "~/") + "\"\(regEx)\""
 	}
 }
 prefix operator /
@@ -57,24 +64,71 @@ prefix operator ~/
 public prefix func ~/(str: String) -> RegEx {return RegEx(regEx: str, caseSensitive: false)}
 
 
-public struct MatchableChars: PrefixMatchable {
-	public var chars: CharacterSet, matchMany: Bool
-	public func matchPrefix(_ str: Substring) -> Range<String.Index>? {
-		let str = matchMany ? str.unicodeScalars : str.prefix(1).unicodeScalars
-		let range = str.startIndex ..< (str.index(where: chars.inverted.contains) ?? str.endIndex)
-		return range.isEmpty ? nil : range
+public struct MatchableChars: ManyPrefixMatchable {
+	public var chars: CharacterSet
+	public func matchPrefix(_ str: Substring) -> String.Index? {
+		let str = str.unicodeScalars.prefix(1)
+		if let matchIndex = str.index(where: chars.contains) {
+			assert(matchIndex == str.startIndex, "only compares with 1 unicodeScalar")
+			return str.index(after: str.startIndex)
+		}
+		return nil
+	}
+	public func matchManyPrefix(_ str: Substring) -> String.Index? {
+		let str = str.unicodeScalars
+		let matchEnd = str.index(where: chars.inverted.contains) ?? str.endIndex
+		if matchEnd == str.startIndex {return nil}
+		return matchEnd
 	}
 	public var description: String {
-		return chars.description
+		return "/\"\(chars.description)\""
 	}
 }
-public prefix func /(chars: CharacterSet) -> MatchableChars {return MatchableChars(chars: chars, matchMany: false)}
+public prefix func /(chars: CharacterSet) -> MatchableChars {return MatchableChars(chars: chars)}
 prefix operator ¬/
-public prefix func ¬/(chars: CharacterSet) -> MatchableChars {return MatchableChars(chars: chars.inverted, matchMany: false)}
+public prefix func ¬/(chars: CharacterSet) -> MatchableChars {return MatchableChars(chars: chars.inverted)}
 prefix operator ¬
-public prefix func ¬(prefix: MatchableChars) -> MatchableChars {return MatchableChars(chars: prefix.chars.inverted, matchMany: false)}
-public postfix func +(prefix: MatchableChars) -> PrefixMatchable {
-	var prefix = prefix
-	prefix.matchMany = true
-	return prefix
+public prefix func ¬(prefix: MatchableChars) -> MatchableChars {return MatchableChars(chars: prefix.chars.inverted)}
+
+
+public struct MatchOptional: PrefixMatchable {
+	var prefix: PrefixMatchable
+	public func matchPrefix(_ str: Substring) -> String.Index? {
+		if let prefix = prefix as? MatchOptional {
+			return prefix.matchPrefix(str)
+		}
+		if let matchEnd = prefix.matchPrefix(str) {
+			return matchEnd
+		}
+		return str.startIndex
+	}
+	public func matchManyPrefix(_ str: Substring) -> String.Index? {
+		return self.matchPrefix(str)
+	}
+	public var description: String {
+		return "(" + prefix.description + ")+"
+	}
 }
+public postfix func .?(prefix: PrefixMatchable) -> MatchOptional {return MatchOptional(prefix: prefix)}
+
+
+public struct MatchMany: ManyPrefixMatchable {
+	var prefix: PrefixMatchable
+	public func matchPrefix(_ str: Substring) -> String.Index? {
+		if let prefix = prefix as? ManyPrefixMatchable {
+			return prefix.matchManyPrefix(str)
+		}
+		var endIndex: String.Index?
+		while let matchEnd = prefix.matchPrefix(str) {endIndex = matchEnd}
+		return endIndex
+	}
+	public func matchManyPrefix(_ str: Substring) -> String.Index? {
+		return self.matchPrefix(str)
+	}
+	public var description: String {
+		return "(" + prefix.description + ")+"
+	}
+}
+public postfix func +(prefix: PrefixMatchable) -> MatchMany {return MatchMany(prefix: prefix)}
+
+public postfix func *(prefix: PrefixMatchable) -> MatchOptional {return MatchOptional(prefix: MatchMany(prefix: prefix))}
